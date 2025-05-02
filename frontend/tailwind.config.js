3f46d7344c759981c6d3f535c176ce01e0c8abd3
Fahad.Mushtaq <fahad.mushtaq@sltn.net>
2025-05-02 16:10:15 +0500

    Convert frontend submodule to regular folder

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            {
+                habitforge: {
+                    "primary": "#3b82f6",
+                    "secondary": "#f3f4f6",
+                    "accent": "#1fb2a6",
+                    "neutral": "#191D24",
+                    "base-100": "#ffffff",
+                    "info": "#3ABFF8",
+                    "success": "#36D399",
+                    "warning": "#FBBD23",
+                    "error": "#F87272",
+                },
+            },
+        ],
+    },
+} 
